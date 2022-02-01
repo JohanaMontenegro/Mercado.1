f9e238dea48f3e86e85cd2c605df3f3b7cdbad78
JohanaMontenegro <92064090+JohanaMontenegro@users.noreply.github.com>
2022-02-01 19:33:43 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Super Mercado Dia");
+console.log("Azucar Ledesma");
+console.log("Yerba Rosamonte");
+console.log("Arroz Gallo");
+console.log("Shampoo Pantene");
+console.log("Pure de tomate Marolio");
